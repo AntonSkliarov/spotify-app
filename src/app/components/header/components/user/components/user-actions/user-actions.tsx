1fd4fe144f0b8a3f50cdae40f1fdf5705bbf59c8
Anton <office.sklyarov@gmail.com>
2021-01-18 11:25:43 +0200

    added logic to userActions component

--- src/app/components/header/components/user/components/user-actions/user-actions.tsx
+++ src/app/components/header/components/user/components/user-actions/user-actions.tsx
@@ -1,14 +1,41 @@
-import React from 'react';
-import { IoChevronDown } from 'react-icons/io5';
+import React, { useState } from 'react';
+import { IoChevronDown, IoChevronUp } from 'react-icons/io5';
 import { DropdownMenu } from './components';
 import './user-actions.sass';
 
-export const UserActions: React.FC = () => (
-  <div className="user-actions">
-    <button className="button user-actions__button" type="button">
-      <IoChevronDown size={25} />
-    </button>
+export const UserActions: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    <DropdownMenu />
-  </div>
-);
+  const onClick = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className="user-actions">
+      {!menuOpen && (
+        <button
+          className="button user-actions__button"
+          type="button"
+          onClick={onClick}
+        >
+          <IoChevronDown size={25} />
+        </button>
+      )}
+
+      {menuOpen && (
+      <>
+        <button
+          className="button user-actions__button"
+          type="button"
+          onClick={onClick}
+        >
+          <IoChevronUp size={25} />
+        </button>
+
+        <DropdownMenu />
+      </>
+      )}
+
+    </div>
+  );
+};
