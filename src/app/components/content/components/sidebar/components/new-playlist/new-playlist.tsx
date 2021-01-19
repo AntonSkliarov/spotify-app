beaa99c646142697da5c3d958537172720895c7d
anton <anton@coderiver.com.ua>
2021-01-19 12:26:25 +0200

    added new-playlist component

---
+++ src/app/components/content/components/sidebar/components/new-playlist/new-playlist.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { IoAddCircleOutline } from 'react-icons/io5';
+import './new-playlist.sass';
+
+export const NewPlaylist = () => (
+  <section className="new-playlist">
+    <a className="new-playlist__link" href="#">
+
+      <span className="new-playlist__icon">
+        <IoAddCircleOutline size={25}/>
+      </span>
+
+      <span className="new-playlist__text">
+        New Playlist
+      </span>
+    </a>
+  </section>
+);
--- src/app/components/content/components/sidebar/sidebar.tsx
+++ src/app/components/content/components/sidebar/sidebar.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Navigation } from './components/navigation';
+import { NewPlaylist } from './components/new-playlist';
 import './sidebar.sass';
 
 export const Sidebar: React.FC = () => (
   <aside className="sidebar">
     <Navigation />
-    <section>New Playlist</section>
+    <NewPlaylist />
     <section>Playing</section>
   </aside>
 );
