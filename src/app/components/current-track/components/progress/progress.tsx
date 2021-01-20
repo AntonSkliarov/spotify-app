32031f94075a7716765ea0f86e1fb02d880bae5b
anton <anton@coderiver.com.ua>
2021-01-20 14:53:59 +0200

    added song-progress component

--- src/app/components/current-track/components/progress/progress.tsx
+++ src/app/components/current-track/components/progress/progress.tsx
@@ -3,14 +3,21 @@ import './progress.sass';
 import currentTrack from '../../../../../api/currentTrack.json';
 import { ITrack } from '../../../../../helpers/interfaces';
 import { msToMinAndSec } from '../../../../../helpers/_functions';
+import { SongProgress } from '../../../ui/song-progress';
 
 const track: ITrack = currentTrack.track;
 
 export const Progress: React.FC = () => (
-  <div>
-    <div>Progress start</div>
-    <div>Progress bar</div>
-    <div>
+  <div className="progress">
+    <div className="progress__start">
+      0:01
+    </div>
+
+    <div className="progress__bar">
+      <SongProgress />
+    </div>
+
+    <div className="progress__end">
       {msToMinAndSec(track.duration)}
     </div>
   </div>
