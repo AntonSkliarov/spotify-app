4f843dcb4e0a683788c5f879ba46d30bdaa8235a
anton <anton@coderiver.com.ua>
2021-01-22 15:21:24 +0200

    added friend and updated friends components

--- src/app/components/content/components/social/components/friends/friends.tsx
+++ src/app/components/content/components/social/components/friends/friends.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Friend } from './components/friend';
 import './friends.sass';
+import userFixture from '../../../../../../../api/userFixture.json';
+import { IFriend } from '../../../../../../../helpers/interfaces';
+
+const { friends } = userFixture.user;
 
 export const Friends: React.FC = () => (
   <>
     <div className="friends">
-      Friends
+      {friends.map((friend: IFriend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
     </div>
 
     <button type="button">Find Friends</button>
