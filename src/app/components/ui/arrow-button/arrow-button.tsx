08d5c50e47463e0e4c69654db24e04aa44cffd22
anton <anton@coderiver.com.ua>
2021-02-04 10:17:50 +0200

    updated arrow-button component

--- src/app/components/ui/arrow-button/arrow-button.tsx
+++ src/app/components/ui/arrow-button/arrow-button.tsx
@@ -16,64 +16,40 @@ interface IArrowButtonProps {
   onClick?: () => void
 }
 
+const arrowButtonIcon = {
+  'back': <IoChevronBack size={25} />,
+  'forward': <IoChevronForward size={25} />,
+  'down': <IoChevronDown size={25} />,
+  'up': <IoChevronUp size={25} />
+};
+
+const arrowButtonTitle = {
+  'back': 'Back',
+  'forward': 'Forward',
+  'down': 'Open menu',
+  'up': 'Close menu'
+};
+
 export const ArrowButton: React.FC<IArrowButtonProps> = ({
   type,
   disabled,
   onClick
 }) => {
+  const getArrowButtonIcon = arrowButtonIcon[type];
+  const getArrowButtonTitle = arrowButtonTitle[type];
 
-  switch (type) {
-    case 'back':
-      return (
-        <button
-          type="button"
-          className={classNames("arrow-button", {
-            "arrow-button_disabled": disabled,
-          })}
-          title='Back'
-        >
-          <IoChevronBack size={25} />
-        </button>
-      );
-
-    case 'forward':
-      return (
-        <button
-          type="button"
-          className={classNames("arrow-button", {
-            "arrow-button_disabled": disabled,
-          })}
-          title='Forward'
-        >
-          <IoChevronForward size={25} />
-        </button>
-      );
-
-    case 'down':
-      return (
-        <button
-          type="button"
-          className="arrow-button arrow-button__down"
-          onClick={onClick}
-          title='Open menu'
-        >
-          <IoChevronDown size={25} />
-        </button>
-      );
-
-    case 'up':
-      return (
-        <button
-          type="button"
-          className="arrow-button arrow-button__up"
-          onClick={onClick}
-          title='Close menu'
-        >
-          <IoChevronUp size={25} />
-        </button>
-      );
-
-    default:
-      return null;
-  }
+  return (
+    <button
+      type="button"
+      className={classNames("arrow-button", {
+        "arrow-button_disabled": disabled,
+        "arrow-button__down": type === 'down',
+        "arrow-button__up": type === 'up',
+      })}
+      onClick={onClick}
+      title={getArrowButtonTitle}
+    >
+      {getArrowButtonIcon}
+    </button>
+  );
 };
